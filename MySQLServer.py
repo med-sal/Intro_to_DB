93eace381450daf72816480704645b6945b2e961
med-sal <41843550+med-sal@users.noreply.github.com>
2025-06-30 00:04:16 +0100

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # Establish connection to MySQL server
@@ -15,7 +14,7 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
         
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error connecting to MySQL: {e}")
     
 finally:
